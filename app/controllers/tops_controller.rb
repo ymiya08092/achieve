4c962dd4cdc330a554a27cff6e63d66b93c554e7
Yuki Miyahara <y_miya@nttpc.co.jp>
2017-12-24 04:57:11 +0000

    init

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,46 +1,5 @@
 class TopsController < ApplicationController
   
   def index
-    @blogs = Blog.all
   end
-  
-  def new
-    @blog = Blog.new
-  end
-  
-  def show
-    @blog = Blog.find(params[:id])
-  end
-  
-  def create
-    @blog = Blog.new(blog_params)
-    if @blog.save
-      redirect_to blogs_path, notice: "ブログを作成しました！"
-    else
-      render 'new'
-    end
-  end
-  
-  def edit
-    @blog = Blog.find(params[:id])
-  end
-  
-  def update
-    @blog = Blog.find(params[:id])
-    if @blog.update(blog_params)
-      redirect_to blogs_path,notice:"ブログを編集しました！"
-    else
-      render 'edit'
-    end
-  end
-  
- private
-  def blog_params
-    params.require(:blog).permit(:title,:content)
-  end
-  
-  def set_blog
-    @blog = Blog.find(params[:id])
-  end
-end
-
+end  
